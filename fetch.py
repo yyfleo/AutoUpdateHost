6a943de5d27955f8a30b668d7af416acef300c8f
Leo Yang <731193292@qq.com>
2020-06-30 11:30:08 +0800

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,6 +1,6 @@
 import socket, time, os
 
-domains=["google.com", "twitter.com", "facebook.com", "youtube.com", "githubusercontent.com", "instagram.com", "pixiv.net", "telegram.org", "steamcommunity.com", "logmein.com"]
+domains=["google.com", "twitter.com", "facebook.com", "youtube.com", "raw.githubusercontent.com", "instagram.com", "pixiv.net", "telegram.org", "steamcommunity.com", "logmein.com"]
 
 if not os.path.exists("docs"):
     os.makedirs("docs")
@@ -14,5 +14,8 @@ with open("docs/hosts", "w") as f:
         domainInfo = socket.getaddrinfo(domain, 443)
         f.write(domainInfo[0][4][0])
         f.write(" ")
+        if not len(domain.split(".")) == 2:
+            temp = domain.split(".")
+            domain = temp[len(temp) - 2] + temp[len(temp) - 1]
         f.write(domain)
         f.write("\n")
